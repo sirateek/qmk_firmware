e9b498e954d63d0ebc40522818c142cff80274eb
Siratee K <sirateek@gmail.com>
2022-03-19 22:54:21 +0700

    Rearrange the layout for maclay

--- keyboards/gmmk/pro/ansi/keymaps/maclay/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/maclay/keymap.c
@@ -4,23 +4,38 @@
 // Ctr + Shift + 4
 #define KC_PSCR_MAC LGUI(LSFT(KC_4))
 
+// Mac lay layout
+//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Del              Rotary
+//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
+//      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgUp
+//      Caps     A        S        D        F        G        H        J        K        L        ;        "                 Enter             PgDn
+//      Sh_L              Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
+//      L_CTR     L_ALT    L_GUI                             SPACE                              R_GUI    FN       R_CTR     Left     Down     Right
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 	[0] = LAYOUT(
-    KC_ESC, KC_BRID, KC_BRIU, KC_F3, KC_F4, BL_DEC, BL_INC, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR_MAC, KC_MUTE,
-    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL,
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_END,
-    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, MO(1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_ESC,   KC_BRID,   KC_BRIU,   KC_F3,  KC_F4,  BL_DEC,  BL_INC,  KC_MPRV,   KC_MPLY,  KC_MNXT,   KC_MUTE,    KC_VOLD,   KC_VOLU,   KC_PSCR_MAC,         KC_MUTE,
+        KC_GRV,      KC_1,      KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,       KC_8,     KC_9,      KC_0,    KC_MINS,    KC_EQL,       KC_BSPC,          KC_DEL,
+        KC_TAB,      KC_Q,      KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,       KC_I,     KC_O,      KC_P,    KC_LBRC,   KC_RBRC,       KC_BSLS,         KC_PGUP,
+        KC_CAPS,     KC_A,      KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,       KC_K,     KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,                        KC_PGDN,
+        KC_LSFT,     KC_Z,      KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,   KC_SLSH,    KC_RSFT,                    KC_UP,          KC_END,
+        KC_LCTL,  KC_LALT,   KC_LGUI,                    KC_SPC,                     KC_RGUI,    MO(1),   KC_RCTL,                     KC_LEFT, KC_DOWN, KC_RGHT
+    ),
 
 
-	[1] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS, NK_TOGG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SPD, RGB_RMOD, RGB_SPI)
+	[1] = LAYOUT(
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS, NK_TOGG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SPD, RGB_RMOD, RGB_SPI
+        )
 };
 
+/// Detect the Knob event
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
+        // If user scroll knob clockwise, Increase volume.
       tap_code(KC_VOLU);
     } else {
+        // Otherwise, Decrease volume.
       tap_code(KC_VOLD);
     }
     return true;
